b54ffc0b58b891518eaca0bc5714ff2604523ff4
Matt Davis <flashmatt@users.noreply.github.com>
2024-08-25 22:52:47 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      '*'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
